9b387f3aaecc51bf4367c52dfd12bae1f71415a6
Taaseen <taas2002@students.ubc.ca>
2022-04-09 17:28:45 -0700

    First Commit with the layout of the game done

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+function button1Clicked() {
+    alert("button1 is clicked!");
+}
+
+function button2Clicked() {
+    alert("button2 is clicked!");
+}
+function button3Clicked() {
+    alert("button3 is clicked!");
+}
+function button4Clicked() {
+    alert("button4 is clicked!");
+}
+function button5Clicked() {
+    alert("butto5 is clicked!");
+}
+function button6Clicked() {
+    alert("button6 is clicked!");
+}
+
+function button7Clicked() {
+    alert("button7 is clicked!");
+}
+function button8Clicked() {
+    alert("button8 is clicked!");
+}
+function button9Clicked() {
+    alert("butto9 is clicked!");
+}
+
+function chooseRandomColour() {
+    
+}
